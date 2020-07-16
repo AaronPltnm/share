103d88b8830a25ddb6a7489a1440c7e8c015aedf
AaronPltnm <aaronlinyong@gmail.com>
2020-07-16 11:31:50 +0800

    commit 1

---
+++ app/src/main/java/com/aaron/share/MainActivity.java
@@ -0,0 +1,18 @@
+package com.aaron.share;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+
+//this is Aaron Chen's project
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
